0d9458d64be8ef85f16b3b7e62f8c52df29be1ee
Fernando Escalona <ferescalona13@gmail.com>
2020-10-14 15:03:30 -0300

    Second commit

---
+++ src/main/java/com/ferescalona/minesweeperapi/MinesweeperapiApplication.java
@@ -0,0 +1,12 @@
+package com.ferescalona.minesweeperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinesweeperapiApplication.class, args);
+    }
+}
