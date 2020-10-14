a6ad7932b478aae81f29aa171458dd044d793895
Fernando Escalona <ferescalona13@gmail.com>
2020-10-14 15:01:49 -0300

    Minesweeper firts commit

---
+++ src/test/java/com/ferescalona/minesweeperapi/MinesweeperapiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ferescalona.minesweeperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
