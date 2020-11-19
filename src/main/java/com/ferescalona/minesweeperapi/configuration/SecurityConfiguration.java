83968984e294954897486e5570afcd62e62bf2f7
Fernando Escalona <ferescalona13@gmail.com>
2020-11-19 17:45:28 -0300

    Disable cors

--- src/main/java/com/ferescalona/minesweeperapi/configuration/SecurityConfiguration.java
+++ src/main/java/com/ferescalona/minesweeperapi/configuration/SecurityConfiguration.java
@@ -10,6 +10,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        http.authorizeRequests().antMatchers("/").permitAll();
+
+        http.cors().and().csrf().disable();
+
 //        http.authorizeRequests().antMatchers("/health").permitAll().anyRequest().authenticated()
 //                .and().httpBasic().and().formLogin();
     }
